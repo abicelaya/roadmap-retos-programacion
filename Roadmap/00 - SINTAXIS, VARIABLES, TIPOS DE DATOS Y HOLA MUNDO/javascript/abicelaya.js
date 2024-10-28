dffaf31448bad01a5eaae76b94554ac8630bbfbb
abril <abicelaya@gmail.com>
2024-10-28 16:59:09 +0100

    ejercicio 00 y 01

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/abicelaya.js
@@ -0,0 +1,24 @@
+// Sitio web oficial de JavaScript: https://developer.mozilla.org/es/docs/Web/JavaScript
+
+// Comentario de una sola linea
+
+/* Comentario
+de varias
+lineas */
+
+// Declaracion de una constante
+const VARIABLE_CONSTANTE = 6; // El valor de una constante no puede cambiarse
+
+// Declaracion de una variable
+let variableLocal = "variable";
+
+// Variables representando tipos de datos primitivos del lenguaje JavaScript
+let entero = 7;
+let float = 3.14;
+let string = "Hola";
+let booleano = true;
+let nulo = null;
+let indefinido = undefined;
+
+// Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+console.log("Hola, JavaScript!");
---
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/javascript/abicelaya.js
@@ -0,0 +1,58 @@
+// Ejemplos utilizando todos los tipos de operadores de JavaScript
+
+// Operador de asignación: asigna un valor a su operando izquierdo basándose en el valor de su operando derecho.
+let num1 = 4;
+let num2 = 3;
+
+// Operador de comparación: compara sus operandos y devuelve un valor lógico en función de si la comparación es verdadera (true) o falsa (false).
+console.log(num1 == num2); // false
+console.log(num1 != num2); // true
+console.log(num1 > num2); // true
+console.log(num1 <= num2); // false
+
+// Operadores aritméticos
+console.log(num1 + num2); // suma
+console.log(num2 - num1); // resta
+console.log(num2 * num1); // multiplicación
+console.log(num2 / num1); // división
+console.log(num2++); // incremento
+console.log(num2--); // decremento
+
+// Operador condicional
+if (edad >= 18) {
+  console.log("Puedes beber");
+} else {
+  console.log("No puedes beber");
+}
+
+// Operador condicional (Ternario)
+let edad = 16;
+let edadParaBeber = edad >= 18 ? "Puedes beber" : "No puedes beber";
+console.log(puedeBeber);
+
+// Operadores lógicos
+let resultado1 = num1 > num2 && num1 > 1;
+console.log(resultado1); // true y true = true
+
+let resultado2 = num1 > num2 || num1 == 1;
+console.log(resultado2); // true y false = true
+
+// Operadores de cadena (concatenación)
+let cadena1 = "hola";
+let cadena2 = " que tal";
+console.log(cadena1 + cadena2); //
+console.log(cadena1 + " JavaScript");
+
+/*
+    Crea un programa que imprima por consola todos los números comprendidos
+    entre 10 y 55 (incluidos), pares, y que no son ni el 16 ni múltiplos de 3.
+*/
+
+let numerosGuardados = 10;
+
+for (let i = 10; i <= 55; i++) {
+  if (i % 2 == 0 && i != 16 && i % 3 != 0) {
+    numerosGuardados += i;
+    console.log(i);
+  }
+}
